61147b8d79d848c77cbe08bf646418763e582ded
Conrad <ccaliari@ikp.tu-darmstadt.de>
2019-02-06 15:16:29 +0100

    added generation of q targets

--- src/QValue.py
+++ src/QValue.py
@@ -11,17 +11,20 @@ class QValue(object):
     """Implementation of the value function.
     Acts as a wrapper for the neural network. All data conversion from Struct objects to input/output tensors is handled here."""
 
-    def __init__(self, network, resolution, numberOfActions, trainer=None, epochs=None):
+    def __init__(self, network, resolution, actionSet, trainer=None, epochs=None):
         """
         Constructor
         :param network: network to use
         :param resolution: resolution of the image representing the state
-        :param numberOfActions: number of actions to be evaluated, matches number of output neurons
+        :param actionSet: number of actions to be evaluated, matches number of output neurons
         :param trainer: trainer to use for training
         :param epochs: number of epochs used for supervised learning on a single training call
         """
+        # store action set
+        self.actionSet = actionSet
+
         # initialize network
-        self.network = network(resolution, numberOfActions)
+        self.network = network(resolution, len(actionSet))
 
         # initialize trainer
         if trainer is not None:
@@ -41,19 +44,19 @@ class QValue(object):
         :return: tensor consisting of Q(s, a)
         """
         netInput = state.image.view(-1)
+        # netInput = state.image
 
         return self.network(netInput)
 
-    def getBestAction(self, state, changesSet):
+    def getBestAction(self, state):
         """
         Get the networks suggestion when being in state
         :param state: current state
-        :param changesSet: set of possible changes (= actions)
         :return: Action object
         """
         bestIndex = self.getActionValues(state).argmax()
 
-        return Action(state, changesSet[bestIndex])
+        return Action(state, self.actionSet[bestIndex])
 
     def train(self, samples, labels):
         """
@@ -66,8 +69,44 @@ class QValue(object):
 
         return
 
+    def genQTargets(self, transitionList, learningRate, discount):
+        """
+        Generate update targets for the network according to the Q-learning rule
+        :param transitionList: list containing observed transitions
+        :param learningRate: learning rate to use
+        :param discount: discount factor of the expected future reward
+        :return: two torch tensors containing samples and corresponding labels
+        """
+        samples, labels = [], []
+
+        for transition in transitionList:
+            # append the state as sample
+            samples.append(transition.action.state.image.view(-1))
+            # samples.append(transition.action.state.image)
+
+            # get current prediction
+            prediction = self.getActionValues(transition.action.state)
+
+            # get maximum q-value for the next state
+            maxQNext = self.getActionValues(transition.nextState).max()
+
+            # find index of the action subject to update
+            for i in range(0, len(self.actionSet)):
+                if torch.equal(transition.action.changes, self.actionSet[i]):
+                    # update current prediction according to Q-learning
+
+                    prediction[i] = prediction[i] + learningRate * (transition.reward + discount * maxQNext - prediction[i])
+                    break
+
+            labels.append(prediction)
+
+        return torch.stack(samples).detach(), torch.stack(labels).detach()  # detach from their graphs so gradients can be calculated properly during training
+
 
 if __name__ == '__main__':
+    from Struct import Transition
+    from FuncApprox.Trainer import SGD
+
     # target resolution
     resolutionTarget, resolutionOversize = 80, 20
     resolution = resolutionTarget + resolutionOversize
@@ -84,6 +123,14 @@ if __name__ == '__main__':
     state, reward = env.react(action)
 
     # initialize value function
-    valueFunction = QValue(FulConI1, resolution, len(actionSet))
+    valueFunction = QValue(FulConI1, resolution, actionSet, SGD, 20)
+
+    # observe a transition
+    action = valueFunction.getBestAction(state)
+    nextState, reward = env.react(action)
+    transition = Transition(action, reward, nextState)
+
+    # generate learning targets
+    samples, labels = valueFunction.genQTargets([transition, transition, transition], 0.5, 0.9)
 
 
