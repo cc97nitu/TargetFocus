4966bc6d71915c8c9d06e8af0c3125f6e58211e7
Conrad <ccaliari@ikp.tu-darmstadt.de>
2019-04-05 15:03:31 +0200

    added step plot

--- src/Plot/Plot.py
+++ src/Plot/Plot.py
@@ -89,3 +89,142 @@ def successPlot(data):
                 plt.close()
 
                 continue
+
+
+def stepsAndSuccess(data):
+    # axes
+    trainEpisodes = data["trainEpisodes"].unique()
+    environmentParameters = data["environmentParameters"].unique()
+
+    successRate = np.empty((len(trainEpisodes), len(environmentParameters)))
+    steps = np.empty((len(trainEpisodes), len(environmentParameters)))
+
+    # selectors
+    networks = data["network"].unique()
+    generators = data["targetGenerator"].unique()
+    learningRates = data["learningRate"].unique()
+
+    epsilon = 0.9
+    selEpsilon = data["epsilon"] == epsilon
+
+    # do the plots
+    for generator in generators:
+        selGenerator = data["targetGenerator"] == generator
+
+        for learningRate in learningRates:
+            selLearningRate = data["learningRate"] == learningRate
+
+            for network in networks:
+                selNetwork = data["network"] == network
+
+                # calculate success rate
+                for i in range(0, len(trainEpisodes)):
+                    for j in range(0, len(environmentParameters)):
+                        selTrainEpisodes = data["trainEpisodes"] == trainEpisodes[i]
+                        selEnvironmentParameters = data["environmentParameters"] == environmentParameters[j]
+
+                        # restrict rows according to selectors
+                        selection = data.loc[
+                            selEpsilon & selGenerator & selLearningRate & selLearningRate & selNetwork & selTrainEpisodes & selEnvironmentParameters]
+
+                        if len(selection) == 0:
+                            print(r"no data for {}, $\alpha$={}, {}, $\epsilon$={}".format(generator, learningRate, network, epsilon))
+                        else:
+                            successRate[i][j] = sum(selection["success"]) / len(selection["success"])
+
+
+                # get steps
+                for i in range(0, len(trainEpisodes)):
+                    for j in range(0, len(environmentParameters)):
+                        selTrainEpisodes = data["trainEpisodes"] == trainEpisodes[i]
+                        selEnvironmentParameters = data["environmentParameters"] == environmentParameters[j]
+
+                        # restrict rows according to selectors
+                        selection = data.loc[
+                            selEpsilon & selGenerator & selLearningRate & selLearningRate & selNetwork & selTrainEpisodes & selEnvironmentParameters]
+
+                        if len(selection) == 0:
+                            print(r"no data for {}, $\alpha$={}, {}, $\epsilon$={}".format(generator, learningRate, network, epsilon))
+                        else:
+                            steps[i][j] = sum(selection["steps"]) / len(selection["steps"])
+
+                # plot it
+                fig, axes = plt.subplots(1, 2, sharey=True)
+
+                axes[0].pcolormesh(environmentParameters, trainEpisodes, successRate)
+                axes[1].pcolormesh(environmentParameters, trainEpisodes, steps)
+
+                fig.suptitle(r"{}, $\alpha$={}, $\epsilon$={}".format(generator, learningRate, epsilon))
+                axes[0].set_title("{}".format(network))
+
+                for ax in axes:
+                    for tick in ax.get_xticklabels():
+                        tick.set_rotation(45)
+
+                    ax.set_xlabel(ax.get_xlabel(), size='x-large')
+                    ax.set_ylabel(ax.get_ylabel(), size='x-large')
+
+                plt.show()
+                plt.close()
+
+                continue
+
+
+def stepPlot(data):
+    # axes
+    trainEpisodes = data["trainEpisodes"].unique()
+    environmentParameters = data["environmentParameters"].unique()
+
+    steps = np.empty((len(trainEpisodes), len(environmentParameters)))
+
+    # selectors
+    networks = data["network"].unique()
+    generators = data["targetGenerator"].unique()
+    learningRates = data["learningRate"].unique()
+
+    epsilon = 0.9
+    selEpsilon = data["epsilon"] == epsilon
+
+    # do the plots
+    for generator in generators:
+        selGenerator = data["targetGenerator"] == generator
+
+        for learningRate in learningRates:
+            selLearningRate = data["learningRate"] == learningRate
+
+            for network in networks:
+                selNetwork = data["network"] == network
+
+                # get steps
+                for i in range(0, len(trainEpisodes)):
+                    for j in range(0, len(environmentParameters)):
+                        selTrainEpisodes = data["trainEpisodes"] == trainEpisodes[i]
+                        selEnvironmentParameters = data["environmentParameters"] == environmentParameters[j]
+
+                        # restrict rows according to selectors
+                        selection = data.loc[
+                            selEpsilon & selGenerator & selLearningRate & selLearningRate & selNetwork & selTrainEpisodes & selEnvironmentParameters]
+
+                        if len(selection) == 0:
+                            print(r"no data for {}, $\alpha$={}, {}, $\epsilon$={}".format(generator, learningRate, network, epsilon))
+                        else:
+                            steps[i][j] = sum(selection["steps"]) / len(selection["steps"])
+
+                # plot it
+                fig, ax = plt.subplots()
+
+                ax.pcolormesh(environmentParameters, trainEpisodes, steps)
+
+                fig.suptitle(r"{}, $\alpha$={}, $\epsilon$={}".format(generator, learningRate, epsilon))
+                ax.set_title("{}".format(network))
+
+                for tick in ax.get_xticklabels():
+                    tick.set_rotation(45)
+
+                ax.set_xlabel(ax.get_xlabel(), size='x-large')
+                ax.set_ylabel(ax.get_ylabel(), size='x-large')
+
+                plt.show()
+                plt.close()
+
+                continue
