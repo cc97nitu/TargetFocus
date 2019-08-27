5491d288e6002a657eae6726c82666d2f47b26d4
Conrad <ccaliari@ikp.tu-darmstadt.de>
2019-08-27 15:59:28 +0200

    working on SQL

--- src/SQL.py
+++ src/SQL.py
@@ -8,12 +8,21 @@ conn = psycopg2.connect(dbname="RL", host="localhost", user="dumpresults", passw
 conn.close()
 
 
-def insert(columnData, blob):
+def insert(columnData, trainBlob):
+    # merge into single dictionary
+    data = {**columnData, "trainBlob": trainBlob}
+
     # establish connection
     conn = psycopg2.connect(dbname="RL", host="localhost", user="dumpresults", password="unsecure")
     db = conn.cursor()
+    insertStatement = "insert into Agents (stateDefinition, actionSet) values (%(stateDefinition)s, %(actionSet)s);"
+    db.execute(insertStatement, columnData)
 
 
-db = conn.cursor()
-db.execute("insert into test (bullshit) values (%s)", ("foo",))
-conn.commit()
+if __name__ == "__main__":
+    # fetch pre-trained agents
+    trainResults = torch.load(
+        "/home/conrad/RL/TempDiff/TargetFocus/src/dump/REINFORCE/6d-states-normalized/ConstantRewardPerStep/6d-norm_9A_RR_Cat3_constantRewardPerStep_2000_agents.tar")
+
+    columnData = trainResults["environmentConfig"]
+    insert(columnData, trainResults)
